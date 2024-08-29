751ac779410a0088d12c684c6366beaca4d85296
Martin Tracey <matracey@outlook.ie>
2024-08-28 19:23:14 -0700

    feat: add main function to cli module

--- src/tracklists_cli/cli.py
+++ src/tracklists_cli/cli.py
@@ -1,3 +1,22 @@
 """
 Contains the entrypoints for the command line tool(s) in this package.
 """
+
+from argparse import ArgumentParser
+
+
+def main():
+    """
+    The main entrypoint for the command line tool.
+
+    :return: None
+    """
+    parser: ArgumentParser = ArgumentParser()
+    parser.add_subparsers(dest="subcommand")
+
+    args = parser.parse_args()
+    subcommand = args.subcommand
+    delattr(args, "subcommand")
+
+    if not subcommand:
+        parser.print_help()
