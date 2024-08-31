f9f27b9e4f3bdc27d7390b3f832d5ddc3d16c4a0
Martin Tracey <matracey@outlook.ie>
2024-08-30 21:35:19 -0700

    feat: add __build_root_parser__ function to cli module

--- src/tracklists_cli/cli.py
+++ src/tracklists_cli/cli.py
@@ -3,6 +3,28 @@ Contains the entrypoints for the command line tool(s) in this package.
 """
 
 from argparse import ArgumentParser
+from typing import Optional
+
+CLI_NAME = "jobHelpers"
+CLI_DESCRIPTION = (
+    "A collection of tools to help with job application submission and management."
+)
+
+
+def __build_root_parser__(
+    prog: Optional[str] = None, description: Optional[str] = None
+) -> ArgumentParser:
+    """
+    Builds the root parser for the command line tool. All commands should inherit from this parser.
+
+    :param prog: The name of the program.
+    :param description: The description of the program.
+    :return: The root parser
+    """
+    parser = ArgumentParser(
+        prog=prog or CLI_NAME, description=description or CLI_DESCRIPTION
+    )
+    return parser
 
 
 def main():
@@ -11,7 +33,7 @@ def main():
 
     :return: None
     """
-    parser: ArgumentParser = ArgumentParser()
+    parser: ArgumentParser = __build_root_parser__()
     parser.add_subparsers(dest="subcommand")
 
     args = parser.parse_args()
