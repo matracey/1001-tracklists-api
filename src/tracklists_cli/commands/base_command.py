e4cfbcc55e39b5d196d875c8e84b390978fb9d58
Martin Tracey <matracey@outlook.ie>
2024-09-01 18:23:20 -0700

    feat: add abstract subcommand destination property to BaseCommand

--- src/tracklists_cli/commands/base_command.py
+++ src/tracklists_cli/commands/base_command.py
@@ -2,8 +2,9 @@
 A module for the BaseCommand class.
 """
 
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
 from argparse import ArgumentParser
+from sys import exit as sysexit
 
 
 class BaseCommand(metaclass=ABCMeta):
@@ -11,6 +12,24 @@ class BaseCommand(metaclass=ABCMeta):
     A base class for any command.
     """
 
+    __abc_error_message__ = "Not implemented by subclass."
+
+    # noinspection PyPropertyDefinition
+    @property
+    @staticmethod
+    @abstractmethod
+    def __subcommand_dest__() -> str:
+        """
+        The destination for the subcommand.
+        """
+        raise NotImplementedError(BaseCommand.__abc_error_message__)
+
     def __init__(self, parser: ArgumentParser, **args):
         self.__args__ = args
         self.__parser__ = parser
+
+        self.__subcommand__ = args.pop(self.__subcommand_dest__)
+
+        if not self.__subcommand__:
+            parser.print_help()
+            sysexit(1)
