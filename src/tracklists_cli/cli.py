8be32d605325ce96756c64e6c20c03cf2a7e1c89
Martin Tracey <matracey@outlook.ie>
2024-09-25 19:12:24 -0700

    feat: add __parse_args_and_invoke__ function to cli module

--- src/tracklists_cli/cli.py
+++ src/tracklists_cli/cli.py
@@ -2,8 +2,10 @@
 Contains the entrypoints for the command line tool(s) in this package.
 """
 
-from argparse import ArgumentParser
-from typing import Optional
+from argparse import ArgumentParser, Namespace
+from typing import Optional, Tuple
+
+from .commands import BaseCommand
 
 CLI_NAME = "jobHelpers"
 CLI_DESCRIPTION = (
@@ -25,6 +27,38 @@ def __build_root_parser__(
         prog=prog or CLI_NAME, description=description or CLI_DESCRIPTION
     )
     return parser
+
+
+def __parse_args_and_invoke__(
+    module: BaseCommand,
+    parser: Optional[ArgumentParser] = None,
+    args: Optional[Namespace] = None,
+):
+    """
+    Invokes the provided module with the provided arguments using the run_command function. If no
+    arguments are provided, they are parsed using the module's build_parser function.
+
+    :param module: The module to invoke. Should have run_command and build_parser functions
+    :param args: The arguments to pass to the module
+    :return: None
+    """
+    if args is None:
+        parser, args = __parse_args__(module)
+    module(parser, **vars(args))
+
+
+def __parse_args__(module: BaseCommand) -> Tuple[ArgumentParser, Namespace]:
+    """
+    Parses the arguments for the provided module using the module's build_parser function.
+
+    :param module: The module to parse arguments for. Should have a build_parser function
+    :return: The parsed arguments as a Namespace
+    """
+    parser: ArgumentParser = __build_root_parser__(
+        module.command_name, module.command_help
+    )
+    module.build_parser(parser)
+    return parser, parser.parse_args()
 
 
 def main():
