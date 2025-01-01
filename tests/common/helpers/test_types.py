2f45371e438129d113c00c79e3aa46e15723be73
Martin Tracey <matracey@outlook.ie>
2025-01-01 16:13:06 +0000

    test: add unit test for dehumanize function to handle comma-separated numbers

--- tests/common/helpers/test_types.py
+++ tests/common/helpers/test_types.py
@@ -18,3 +18,8 @@ def test_dehumanize_should_handle_million_symbols():
 def test_dehumanize_should_handle_billion_symbols():
     assert dehumanize("3b") == str(3_000_000_000)
     assert dehumanize("3.5b") == str(3_500_000_000)
+
+
+def test_dehumanize_should_handle_commas():
+    assert dehumanize("1,000") == str(1_000)
+    assert dehumanize("1,000,000") == str(1_000_000)
