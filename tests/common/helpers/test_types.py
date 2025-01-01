5e031374517c05306e35292e6ae92ed1c496d7d8
Martin Tracey <matracey@outlook.ie>
2025-01-01 16:13:06 +0000

    test: add unit test for dehumanize function to handle million symbols

--- tests/common/helpers/test_types.py
+++ tests/common/helpers/test_types.py
@@ -8,3 +8,8 @@ from src.common import dehumanize
 def test_dehumanize_should_handle_thousand_symbols():
     assert dehumanize("1k") == str(1_000)
     assert dehumanize("1.2k") == str(1_200)
+
+
+def test_dehumanize_should_handle_million_symbols():
+    assert dehumanize("2m") == str(2_000_000)
+    assert dehumanize("2.5m") == str(2_500_000)
