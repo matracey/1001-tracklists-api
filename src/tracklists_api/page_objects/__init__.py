9075e0aef33a31effdd0e6dcfd5e1b3def9f9be3
Martin Tracey <matracey@outlook.ie>
2025-01-01 17:17:31 +0000

    feat: add HomePageObject class for fetching home page data

--- src/tracklists_api/page_objects/__init__.py
+++ src/tracklists_api/page_objects/__init__.py
@@ -1,1 +1,2 @@
 from .base_page_object import BasePageObject
+from .home_page_object import HomePageObject
---
+++ src/tracklists_api/page_objects/home_page_object.py
@@ -0,0 +1,9 @@
+from .base_page_object import BasePageObject
+
+
+class HomePageObject(BasePageObject):
+    """
+    A page object class for the home page.
+    """
+
+    __path__ = "/"
