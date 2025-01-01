12030c1ae30201d2b50fe6653c5bb4d7f369b9e7
Martin Tracey <matracey@outlook.ie>
2025-01-01 17:18:02 +0000

    feat: add get_sidebar_sections method to HomePageObject

--- src/tracklists_api/page_objects/home_page_object.py
+++ src/tracklists_api/page_objects/home_page_object.py
@@ -1,3 +1,5 @@
+from typing import Iterator
+
 from .base_page_object import BasePageObject
 
 
@@ -7,3 +9,19 @@ class HomePageObject(BasePageObject):
     """
 
     __path__ = "/"
+
+    __details_selector__: str = "details"
+    __summary_selector__: str = "summary"
+    __summary_text_selector__: str = f"{__summary_selector__}::text"
+    __sidebar_section_titles_selector__: str = (
+        f"{__details_selector__} > {__summary_text_selector__}"
+    )
+
+    def get_sidebar_sections(self) -> Iterator[str]:
+        """
+        Returns a list of sidebar sections.
+        """
+        for section in self._selector_.css(
+            self.__sidebar_section_titles_selector__
+        ).getall():
+            yield section.strip()
